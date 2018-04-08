07da70a9492bae8d6708bead492834424e009404
lifeofpython <alexyzhans@gmail.com>
2018-04-08 16:43:56 +0800

    Initial commit

---
+++ src/quickstart/HelloWorld.java
@@ -0,0 +1,13 @@
+package quickstart;
+
+/**
+ * Created by hyz on 2018/4/8
+ */
+
+
+public class HelloWorld {
+    public static void main (String[] args){
+
+        System.out.println("Hello, World");
+    }
+}
